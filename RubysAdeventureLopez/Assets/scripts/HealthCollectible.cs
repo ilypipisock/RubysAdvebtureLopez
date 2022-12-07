3651c04ee1453a4ee9129ce5c5079a0168782784
ilypipisock <sebastianlopezsecucv@gmail.com>
2022-12-07 11:44:09 -0800

    i did the healing
    it was reallly fun

---
+++ RubysAdeventureLopez/Assets/scripts/HealthCollectible.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        RubysController controller = other.GetComponent<RubysController>();
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- RubysAdeventureLopez/Assets/scripts/RubysController.cs
+++ RubysAdeventureLopez/Assets/scripts/RubysController.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class RubysController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Creating the two variables that use the pre built axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate()
+    {
         //create the movment vector
         Vector2 position = transform.position;
 
@@ -28,5 +41,13 @@ public class RubysController : MonoBehaviour
 
         //Set the upadte position
         transform.position = position;
+
+        rigidbody2d.MovePosition(position);
+    }
+       
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
